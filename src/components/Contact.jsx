c4a89d6e31ee929aaba012401e5d68051e16b2d0
Avatapalli Vishnuvardhan Rao <avatapallivishnu@gmail.com>
2023-11-26 21:59:09 +0530

    installed packages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,13 +5,14 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact:</p>
                 <p className='text-gray-300 py-4'>Shoot me an mail-<email>avatapallivishnu@gmail.com</email></p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+            {/* <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name="message" rows="5" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+         */}
         </form>
     </div>
   )
