0e46dc1d76e1cfef39ebb873b32f56dab13b8afc
Avatapalli Vishnuvardhan Rao <66308263+vishnu2407@users.noreply.github.com>
2025-06-08 21:37:00 +0530

    Update MyPortfolio.jsx

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -32,7 +32,7 @@ export default function MyPortfolio() {
         {data?.portfolio?.map((item, index) => (
           <div key={index} className="portfolio--section--card">
             <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
+{/*               <img src={item.src} alt="Placeholder" /> */}
             </div>
             <div className="portfolio--section--card--content">
               <div>
